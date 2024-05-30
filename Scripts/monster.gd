50418b64f804dc69dad674b21d437ad03bd6e3aa
Donovan Liu <144486880+donovanliu1@users.noreply.github.com>
2024-05-30 15:34:59 -0400

    Add files via upload

--- Scripts/monster.gd
+++ Scripts/monster.gd
@@ -1,6 +1,9 @@
-extends Node3D
+extends CharacterBody3D
 
 @export var player : CharacterBody3D
+@onready var nav_agent = $NavigationAgent3D
+
+var SPEED = 5.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,8 +11,15 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-
-
-	position = position + (player.position - position) / 100
+func _physics_process(delta):
+	var current_location = global_transform.origin
+	var next_location = nav_agent.get_next_path_position()
+	var new_velocity = (next_location - current_location).normalized() * SPEED
+	velocity = velocity.move_toward(new_velocity, .5)
+	
+	move_and_slide()
 	print("monster position: ", position)
+
+func update_target_location(target_location):
+	nav_agent.target_position = target_location
+
